dd09c1545925269ac511cae97f810b04afc048a0
RaúlIzquierdoPortatil <instalacion@mail.com>
2024-04-17 20:44:14 +0200

    master/primer commit

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 function initMap(){
 
+    // contenido de las etiquetas:
+    const tag_content = {
+        img: '',
+        username: '',
+        description: '',
+        links: '',
+        song: '',
+        address: ''
+    }
+
+
     //centramos el mapa en un lugar específico (Barcelona)
-    const centerMap = {lat: 41.3926386, long: 2.0577881}
+    const centerMap = {lat: 41.3926386, lng: 2.0577881}
 
     //determinamos por defecto la posición de la ventana del mapa en nuestro navegador
-    const mapOption = {
+    const mapOptions = {
         center: centerMap,
-        zoom: 10
+        zoom: 10,
+        disableDefaultUI: true
     }
 
 
