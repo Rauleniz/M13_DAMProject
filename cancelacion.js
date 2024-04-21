89f923d6bf7ef9a3eca980d9583448f166518e04
RaúlIzquierdoPortatil <instalacion@mail.com>
2024-04-21 17:02:04 +0200

    html y css_home page

---
+++ cancelacion.js
@@ -0,0 +1,13 @@
+
+
+
+
+// volver a miperfil.html
+document.addEventListener("DOMContentLoaded", function() {
+    const noButton = document.getElementById('no-btn');
+
+    noButton.addEventListener('click', function() {
+        // Redireccionar al usuario a la página "miperfil.html"
+        window.location.href = 'miperfil.html';
+    });
+});
