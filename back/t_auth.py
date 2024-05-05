44cb28d0b2c138e244b0411e54099077c1fe0585
RaúlIzquierdoPortatil <instalacion@mail.com>
2024-05-05 20:02:23 +0200

    feature/crud

--- back/t_auth.py
+++ back/t_auth.py
@@ -1,31 +1,35 @@
-import unittest
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token
+from .models import Usuario
+from db import get_database_connection
 
-from flask import jsonify
-from auth import verificar_credenciales, verificar_credenciales_decorador
+app = Flask(__name__)
 
-class TestAuth(unittest.TestCase):
-    def test_verificar_credenciales(self):
-        # Prueba con credenciales válidas
-        usuario_valido = verificar_credenciales('usuario', 'contraseña')
-        self.assertIsNotNone(usuario_valido)
-        
-        # Prueba con credenciales inválidas
-        usuario_invalido = verificar_credenciales('usuario', 'contraseña_incorrecta')
-        self.assertIsNone(usuario_invalido)
-        
-    def test_verificar_credenciales_decorador(self):
-        # Prueba con una función de prueba simulada
-        @verificar_credenciales_decorador
-        def funcion_prueba(usuario):
-            return 'Hola, {}'.format(usuario['username'])
-        
-        # Prueba con credenciales válidas
-        resultado_valido = funcion_prueba({'username': 'usuario', 'id': 1})
-        self.assertEqual(resultado_valido, 'Hola, usuario')
-        
-        # Prueba con credenciales inválidas
-        resultado_invalido = funcion_prueba(None)
-        self.assertEqual(resultado_invalido, jsonify({'mensaje': '**/Credenciales incorrectas'}), 401)
+# Configurar la clave secreta para firmar tokens JWT
+app.config['JWT_SECRET_KEY'] = 'DamM13&Proj3ct'
+
+# Inicializar la extensión JWTManager con la aplicación Flask
+jwt = JWTManager(app)
+
+@app.route('/auth/register', methods=['POST'])
+def register():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    # Verificar si el usuario ya existe en la base de datos
+    if Usuario.query.filter_by(username=username).first():
+        return jsonify({'message': 'El usuario ya existe'}), 400
+
+    # Crear un nuevo usuario
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO usuarios (username, password) VALUES (%s, %s)", (username, password))
+    connection.commit()
+
+    # Crear y devolver el token de acceso
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
 
 if __name__ == '__main__':
-    unittest.main()
+    app.run(debug=True)
